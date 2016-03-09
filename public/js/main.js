161e29087f378e5328460fccb4aa5d34759b0b6e
Stian Surén <stian.suren@bekk.no>
2016-03-09 16:21:39 +0100

    Fjernet node-trello wrapper og la til superagent

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,39 @@
 // Run on load
 $(window).load(function(){
 
-  //Get the Tilgjengelig list
-  $.get('/trelloList', function(data){
-    console.log(data);
-  })
-
   // Get cards from Tilgjenglig list
   $.get('/trelloCards', function(data){
     for (object in data){
-      console.log (data[object]);
       renderCard (data[object], object);
     }
   })
+
+  var loanPerson = {};
+  loanPerson.name = "Aslak";
+  loanPerson.state = "complete";
+  loanPerson.pos = "bottom";
+
+  console.log (loanPerson);
+  console.log(JSON.stringify(loanPerson));
+
+  // JSON.stringify(loanPerson);
+
+  // $.post('postName', loanPerson)
+  //   .done(function(data){
+  //     console.log(data);
+  //   });
+
+  jQuery.ajax ({
+     url: '/postName',
+     type: "POST",
+     data: JSON.stringify(loanPerson),
+     dataType: "json",
+     contentType: "application/json; charset=utf-8",
+     success: function(data){
+         console.log(data);
+     }
+  });
+
 
 });
 
@@ -27,5 +48,5 @@ function renderCard (card, order) {
   else {
     $('#' + order).append('<p class="card card-header">' + card.desc + '</p>');
   };
-}
+};
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var Trello = require("node-trello");
+var request = require('superagent');
+var bodyParser = require('body-parser');
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -12,6 +13,9 @@ app.set('view engine', 'ejs');
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
+// use body-parser to populate json request bodies
+app.use(bodyParser.json());
+
 // set the home page route
 app.get('/', function(req, res) {
 
@@ -20,32 +24,54 @@ app.get('/', function(req, res) {
 });
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('Gadgetlab is running on http://localhost:' + port);
 });
+
+// Trello api
+
+// Base URL to be added to every HTTP-request
+var trelloBaseURL = 'http://api.trello.com';
 
 //Get permission to access my Trello account
-var readToken = 'd142d27de0242d5245532fd416c07f84843ce3de64fe75d8d245776fbaa40508';
+var trelloHeader = {'key':'d121cf6f29cf938fd16890234a8d03fa', 'token':'d142d27de0242d5245532fd416c07f84843ce3de64fe75d8d245776fbaa40508'}
 
-var t = new Trello("d121cf6f29cf938fd16890234a8d03fa", readToken);
-
-app.get('/trelloList', function(req, res) {
-
-	var tilgjengeligList = '56a27fb6a5d9d612e1c88341';
-
-   	t.get("/1/lists/" + tilgjengeligList, function(err, data) {
-		if (err) throw err;
-		res.json (data);
-	});
-
-});
-
+// Get all cards the Tilgjengelig list from Trello
 app.get('/trelloCards', function(req, res) {
 
-	var cards = '/1/lists/56a27fb6a5d9d612e1c88341/cards';
+	var cards = trelloBaseURL + '/1/lists/56a27fb6a5d9d612e1c88341/cards';
 
-   	t.get(cards, function(err, data) {
-		if (err) throw err;
-		res.json (data);
-	});
+	request
+	   .get(cards)
+	   .set('Content-Type', 'application/json')
+	   .query(trelloHeader)
+	   .end(function(_err, _res){
+			res.json(_res.body);
+   		});
 
 });
+
+// Post an element to a checklist on a card in Trello
+app.post('/postName', function(req, res) {
+
+	var checkList = trelloBaseURL + '/1/checklists/56def8fa103d4644bb226f4e/checkItems';
+
+	console.log(req.body);
+
+	request
+	   .post(checkList)
+	   // .set('Content-Type', 'application/json')
+	   .query(trelloHeader)
+	   .send(req.body)
+	   .end(function(_err, _res){
+	   		console.log(_res.body);
+			res.json(_res.body);
+   		});
+   	
+});
+
+
+
+
+
+
+
