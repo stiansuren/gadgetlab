c96f93b8922d35e1ed587d21469c5c1da4d6a85e
Stian Surén <stian.suren@bekk.no>
2016-03-13 16:13:35 +0100

    Kan hente ut lånetager og poste nye lånetagere

--- public/js/main.js
+++ public/js/main.js
@@ -8,59 +8,54 @@ $(window).load(function(){
     }
   })
 
-  var loanPerson = {};
-  loanPerson.name = "Rune";
-  loanPerson.checked = "true";
-  // loanPerson.pos = "bottom";
-
-  // JSON.stringify(loanPerson);
-
-  // $.post('postName', loanPerson)
-  //   .done(function(data){
-  //     console.log(data);
-  //   });
-
-  var newCard = {};
-  newCard.name = "Enda et kort";
-  newCard.due = "null";
-
-  var cardOne = {"name":"Sjekk ut dette", "due":"null"};
-
-  jQuery.ajax ({
-     url: '/postName',
-     type: "POST",
-     data: JSON.stringify(loanPerson),
-     // dataType: "json",
-     contentType: "application/json; charset=utf-8",
-     success: function(data){
-         console.log('suksess');
-     }
-  });
-
-  jQuery.ajax ({
-     url: '/postCard',
-     type: "POST",
-     data: JSON.stringify(cardOne),
-     // dataType: "json",
-     contentType: "application/json; charset=utf-8",
-     success: function(data){
-         console.log('suksess');
-     }
-  });
-
-
 });
 
 // Render the Trello cards
 function renderCard (card, order) {
+
+  // Add container and header
   $('#cards').append('<div class="card" id="' + order + '"></div>');
   $('#' + order).append('<h3 class="card card-header">' + card.name + '</h3>');
 
+  // Add description
   if (card.desc === ""){
     $('#' + order).append('<p class="card card-header">Denne gadgeten har ingen beskrivelse, men den er uansett awesome. Google!</p>');
   }
   else {
     $('#' + order).append('<p class="card card-header">' + card.desc + '</p>');
   };
+
+  // Check if the gadget is available and add options to either loan or to be on a waiting list
+  if(card.badges["checkItems"] === card.badges["checkItemsChecked"]){
+    $('#' + order).append('<h4>Tilgjenglig</h4>');
+    $('#' + order).append('<button class="btn" type="submit">Låne ' + card.name + '</button>');
+  }
+  else{
+
+    $.get('/currentLoaner/' + card.idChecklists[0], function(data){
+      setCurrentLoaner(data);
+    })
+
+    function setCurrentLoaner(data){
+      var currentLoaner = data.checkItems[data.checkItems.length - 1].name;
+      $('#' + order).append('<h4>Lånes av ' + currentLoaner + '</h4><button class="btn venteliste" type="submit">Venteliste</button>');
+    };
+  }
+};
+
+function loanGadget (input){
+  var loanPerson = {};
+  loanPerson.name = input;
+  loanPerson.checked = "true";
+
+  jQuery.ajax ({
+       url: '/postName',
+       type: "POST",
+       data: JSON.stringify(loanPerson),
+       contentType: "application/json; charset=utf-8",
+       success: function(data){
+           console.log('suksess');
+       }
+    });
 };
 
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.listen(port, function() {
 // Trello api
 
 // Base URL to be added to every HTTP-request
-var trelloBaseURL = 'http://api.trello.com';
+var trelloBaseURL = 'https://api.trello.com';
 
 const trelloSecret = 'ba1591534f6e12cc443b379ce1ff4519b4a882347b3cf0a38f9fcc9c4056237d';
 const trelloKey = 'd121cf6f29cf938fd16890234a8d03fa';
@@ -38,7 +38,6 @@ const trelloToken = 'd142d27de0242d5245532fd416c07f84843ce3de64fe75d8d245776fbaa
 
 //Get permission to access my Trello account
 var trelloHeader = {'oauth_consumer_key': trelloKey, 'oauth_token': trelloToken, 'secret': trelloSecret};
-var testHeader = 'oauth_consumer_key=d121cf6f29cf938fd16890234a8d03fa&oauth_token=d142d27de0242d5245532fd416c07f84843ce3de64fe75d8d245776fbaa40508&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1457620041&oauth_nonce=pfimA3&oauth_version=1.0&oauth_signature=Nbvv86aCnnbkxZtEmzuqc4r4G6I='
 
 // Get all cards the Tilgjengelig list from Trello
 app.get('/trelloCards', function(req, res) {
@@ -55,12 +54,23 @@ app.get('/trelloCards', function(req, res) {
 
 });
 
+app.get('/currentLoaner/:id', function(req, res) {
+
+	var cards = trelloBaseURL + '/1/checklists/' + req.params.id;
+
+	request
+	   .get(cards)
+	   .set('Content-Type', 'application/json')
+	   .query(trelloHeader)
+	   .end(function(_err, _res){
+			res.json(_res.body);
+   		});
+});
+
 // Post an element to a checklist on a card in Trello
 app.post('/postName', function(req, res) {
 
 	var checkList = trelloBaseURL + '/1/checklists/56def8fa103d4644bb226f4e/checkItems';
-
-	// console.log(req.body);
 
 	request
 	   .post(checkList)
@@ -68,26 +78,6 @@ app.post('/postName', function(req, res) {
 	   .send(req.body)
 	   .query(trelloHeader)
 	   .end(function(_err, _res){
-	   		// console.log(_res.body);
-			res.json(_res.body);
-   		});
-   	
-});
-
-// Post an element to a checklist on a card in Trello
-app.post('/postCard', function(req, res) {
-
-	var cards = trelloBaseURL + '/1/lists/56a27fb6a5d9d612e1c88341/cards';
-
-	console.log(req.body);
-
-	request
-	   .post(cards)
-	   .set('Content-Type', 'application/json')
-	   .send(req.body)
-	   .query(trelloHeader)
-	   .end(function(_err, _res){
-	   		// console.log(_res.body);
 			res.json(_res.body);
    		});
    	
