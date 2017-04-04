28fad67efa24b64e858ecf8441423b9a738e694e
Ingar Kvalheim <ingarkvalheim@mac-ingarkvalheim.local>
2017-04-04 18:08:01 +0200

    Endret mottaker av mail

--- app/components/BorrowButton.jsx
+++ app/components/BorrowButton.jsx
@@ -6,7 +6,7 @@ class BorrowButton extends Component {
 	render(){
 		return (
 			<a href="mailto:ingar.kvalheim@bekk.no?subject=Jeg%20er%20gira%20på%20en%20gadget!&body=Hei!%0A%0AJeg vil gjerne låne .., fra .. og til ... Jeg lover å ha det gøy mens jeg låner den, at jeg leverer den tilbake til Gadget Lab-skapet og at jeg sier ifra til deg når jeg er ferdig." className="btn">
-				<h4>Send forespørsel</h4>
+				<h4>Send forespørsel.</h4>
 			</a>
 		)
 	}
